5d0b6c14a3bed1189248ddeaf8ac4c4589a0354a
EmilG <emilgalikhanov44@gmail.com>
2023-08-01 00:07:51 +0300

    feat: improved code readability

--- get_gps_coordinates.py
+++ get_gps_coordinates.py
@@ -1,4 +1,4 @@
-from typing import NamedTuple 
+from typing import NamedTuple
 from geopy.geocoders import Nominatim
 from exceptions import CantGetCoordinates
 from config import USE_ROUNDED_COORDINATES
@@ -8,27 +8,24 @@ class Coordinates(NamedTuple):
     longitude: float
     latitude: float
 
-class Location(NamedTuple):
+class _Location(NamedTuple):
     address: str
     latitude: float
     longitude: float
     
-
+    
 def get_coordinates() -> Coordinates:
+    adress, latitude, longitude = _get_location_by_street_city_country()
+    """Returns rounded coordinates"""
+    if USE_ROUNDED_COORDINATES == True:
+        latitude, longitude = map(lambda x: round(x, 2), [latitude, longitude])
+        return Coordinates(longitude=longitude, latitude=latitude)
+
+def _get_location_by_street_city_country() -> _Location:
     geolocator = Nominatim(user_agent='My App')
     location = geolocator.geocode({'street': 'Долгоозёрная', 'city': 'Санкт-Петербург', 'country': 'Россия'}, language='ru')
-    if location is not None:
-        """Returns selected coordinates"""
-        if USE_ROUNDED_COORDINATES == True:
-            longitude, latitude = map(lambda x: round(x,2), [location.longitude, location.latitude])
-            return Coordinates(longitude=longitude, latitude=latitude) 
-    else: 
-        raise CantGetCoordinates  ## Создаём свой класс исключений в файле Exceptions.py
-
-def _get_location_by_street_city_country() -> Location:
-    geolocator = Nominatim(user_agent='My App')
-    location = geolocator.geocode({'street': 'Долгоозёрная', 'city': 'Санкт-Петербург', 'country': 'Россия'}, language='ru')
-    return Location(address=location.address, latitude=location.latitude, longitude=location.longitude)
-
-
-print(_get_location_by_street_city_country().longitude, _get_location_by_street_city_country().latitude)
+    if location is not None: 
+        return _Location(address=location.address, latitude=location.latitude, longitude=location.longitude)
+    else:
+        raise CantGetCoordinates
+        
